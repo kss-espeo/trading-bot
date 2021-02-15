ea47caf532566d3de1665a3766788c6825ee770e
kss-espeo <krzysztof.spisak.spisacki@gmail.com>
2021-02-15 16:17:52 +0100

    added test trading bot supporting BigDecimal

---
+++ src/SimpleTradingBot.ts
@@ -0,0 +1,13 @@
+import TradingBot from "./TradingBot";
+
+const bigdecimal = require("bigdecimal");
+
+class SimpleTradingBot implements TradingBot{
+
+  async trade(balance: {ETH, USD}): Promise<void> {
+    balance.ETH = balance.ETH.add(new bigdecimal.BigDecimal('0.01'));
+    balance.USD = balance.USD.add(new bigdecimal.BigDecimal('1'));
+  }
+}
+
+export default SimpleTradingBot;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello world');
+import SimpleTradingBot from "./SimpleTradingBot";
+import TradingBot from "./TradingBot";
+
+const bigdecimal = require("bigdecimal");
+
+let BALANCE = {
+  ETH: new bigdecimal.BigDecimal('10'),
+  USD: new bigdecimal.BigDecimal('2000')
+};
+
+const delayMs = 1000;
+const bot: TradingBot = new SimpleTradingBot();
+
+(async () => {
+  let delayTicks = 0;
+  while (true) {
+    await bot.trade(BALANCE);
+
+    console.log('.');
+    await delay(delayMs);
+    if (++delayTicks == 5) {
+      console.log(`ETH balance: ${BALANCE.ETH} , USD balance: ${BALANCE.USD}`);
+      delayTicks = 0;
+    }
+  }
+})();
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
